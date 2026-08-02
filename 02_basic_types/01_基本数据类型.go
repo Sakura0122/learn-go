5b80b9afcf9de648388f593a76168bb5d85248c3
sakura <11854578@qq.com>
2026-08-02 21:16:50 +0800

    feat(basic_types): 添加基本数据类型示例代码

---
+++ 02_basic_types/01_基本数据类型.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a uint8 = 255
+	b := 256
+
+	var c float32 = 3.14
+	d := 3.1415
+
+	var e byte = 'e'
+	f := 'f'
+	var g rune = '中'
+	h := '中'
+
+	var i bool = true
+	j := false
+
+	var k string = "hello"
+	l := "world"
+
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+	fmt.Println(d)
+	fmt.Println(e)
+	fmt.Println(f)
+	fmt.Println(g)
+	fmt.Println(h)
+	fmt.Println(i)
+	fmt.Println(j)
+	fmt.Println(k)
+	fmt.Println(l)
+}
